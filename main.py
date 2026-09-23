0ef60b258a81c15b7fd1193d8ded3bd3a547020c
Ata <131000146+Fltuir@users.noreply.github.com>
2026-09-23 23:14:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import ts3
-from ts3.query import TS3ConnectionError
 
 # اطلاعات اتصال به سرور تیم‌اسپیک شما
 HOST = "46.38.138.68"
@@ -14,7 +13,7 @@ try:
     with ts3.query.TS3Connection(HOST, PORT) as ts3conn:
         # لاگین با اطلاعات کوئری
         ts3conn.login(client_login_name=USER, client_login_password=PASS)
-        print(" اتصال موفقیت‌آمیز بود! ربات به راحتی توانست به سرور لاگین کند.")
+        print("اتصال موفقیت‌آمیز بود! ربات به راحتی توانست به سرور لاگین کند.")
         
         # انتخاب مجازی سرور (آیدی 1 یا پیش‌فرض)
         ts3conn.use(sid=1)
@@ -22,9 +21,7 @@ try:
         # دریافت اطلاعات پایه سرور برای تست
         resp = ts3conn.hostinfo()
         server_name = resp[0].get("virtualserver_name", "نامشخص")
-        print(f" نام سرور تیم‌اسپیک شما: {server_name}")
+        print(f"نام سرور تیم‌اسپیک شما: {server_name}")
 
-except TS3ConnectionError as e:
-    print(f" خطای اتصال به سرورکوئری: {e}")
 except Exception as e:
-    print(f" خطای ناشناخته رخ داد: {e}")
+    print(f"خطا در اتصال به سرورکوئری: {e}")
